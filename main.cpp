c133e03af9ea383ac341599f5b6da991ebadf40a
jotask <jota_sk@hotmail.com>
2016-02-29 16:10:28 +0000

    main class with testing and CMake

--- main.cpp
+++ main.cpp
@@ -1,8 +1,20 @@
-#include <iostream>
+//
+// Created by Jose Vives on 29/02/2016.
+//
 
-using namespace std;
+#include "World.h"
+#include "Robot.h"
 
 int main() {
-    cout << "Hello, World!" << endl;
+
+    World* world;
+    world = new World();
+
+    Robot* robot;
+    robot = new Robot(world);
+
+    // Destroy everything
+    world -> ~World();
+
     return 0;
 }
