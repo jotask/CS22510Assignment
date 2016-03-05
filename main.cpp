e8b73005f489930984c307f1b78350a545910349
Jose Vives <jota_sk@hotmail.com>
2016-03-05 17:10:08 +0000

    refactored and ready for the simulation

--- main.cpp
+++ main.cpp
@@ -2,149 +2,34 @@
 // Created by Jose Vives on 29/02/2016.
 //
 
-#include <fstream>
 #include <iostream>
-#include <sstream>
-#include <vector>
 #include "World.h"
 #include "Robot.h"
-
-const std::string path = "data/";
-const std::string posesFilename = "poses.txt";
-const std::string rangesFilename = "ranges.txt";
+#include "Simulation.h"
 
 using namespace std;
 
-vector< vector<double> > readPoses(){
-
-    vector< vector<double> > posesReaded;
-
-    ifstream infile(path + posesFilename);
-    if(!infile.is_open()){
-        std::cout << "file not found" << std::endl;
-        infile.close();
-        return posesReaded;
-    }
-
-    std::string line;
-    while(std::getline(infile, line)){
-        std::istringstream iss(line);
-
-        vector<double> tmp(3);
-        for(int i = 0; i < 3; i++){
-            iss >> tmp[i];
-        }
-
-        posesReaded.push_back(tmp);
-    }
-
-    infile.close();
-
-    return posesReaded;
-
-}
-
-vector< vector<double> > readRanges() {
-
-    vector< vector<double> > rangesReaded;
-
-    std::ifstream infile(path + rangesFilename);
-    if(!infile.is_open()){
-        std::cout << "file not found" << std::endl;
-        infile.close();
-        return rangesReaded;
-    }
-
-    std::string line;
-    while(std::getline(infile, line)){
-
-        std::istringstream iss (line);
-
-        vector<double> tmp(Robot::NUMBER_SENSORS);
-
-        for(int i = 0; i < Robot::NUMBER_SENSORS; i++){
-            iss >> tmp[i];
-        }
-
-        rangesReaded.push_back(tmp);
-
-    }
-
-    infile.close();
-
-    return rangesReaded;
-
-}
-
-void setRobot(Robot* robot, vector<double> p, vector<double> *r){
-    // FIXME convert to grid coordinates
-    int x, y, orientation;
-
-    World* world;
-    world = robot -> getWorld();
-
-    x = world -> convertToWorldCoordinates(p[0]);
-    y = world -> convertToWorldCoordinates(p[1]);
-
-//    cout << "x:" << p[0] << " y:" << p[1] << endl;
-//    cout << "x:" << x << " y:" << y << endl << endl;
-
-    orientation = (int)p[2];
-    robot -> setPosition(x, y);
-    robot -> setOrientation(orientation);
-
-    // Set the values for all the sensors for the robot, for simulation
-    Sensor* sensors;
-    robot -> getSensors();
-    for(int i = 0; i < Robot::NUMBER_SENSORS; i++){
-        double tmp;
-        tmp = r -> at(i);
-        sensors -> setRead(tmp);
-    }
-}
-
-void information(World* world, double test){
-    cout << "O: " << test << " C: " << world -> convertToWorldCoordinates(test) << endl;
-}
-
 int main() {
 
-//    vector< vector<double> > poses;
-//    poses = readPoses();
-//
-//    vector< vector<double> > ranges;
-//    ranges = readRanges();
-//
-//    if(poses.size() != ranges.size()){
-//        cout << "something is wrong" << endl;
-//        return -1;
-//    }
+    const char posesFilename[] = "data/poses.txt";
+    const char rangesFilename[] = "data/ranges.txt";
 
-    World* world;
-    world = new World();
+    World world;
 
-    Robot* robot;
-    robot = new Robot(world);
+    Robot robot (world);
 
-//    while(!poses.empty() && !ranges.empty()){
-//
-////        world -> printWorld();
-//
-//        setRobot(robot, poses.front(), &ranges.front());
-//        poses.erase(poses.begin());
-//        ranges.erase(ranges.begin());
-//
-//        robot -> update();
-//
-//    }
+    Simulation simulation (robot, posesFilename, rangesFilename);
 
-    for(int i = 0; i < World::WORLD_WIDTH; i++){
-        for(int j = 0; j < World::WORLD_HEIGHT; j++){
-            world -> setValueAt(i, j, true);
-        }
+    if(simulation.hasToSimulate()){
+        cout << "Error reading the files for the simulation" << endl;
+        return 1;
     }
 
-    world -> printWorld();
+    while(!simulation.isFinishedSimulation()){
+        cout << "simulation in process" << endl;
+    }
+
+    cout << "Simulation finished" << endl;
 
     return 0;
 
