f0077c509653ab1069bc5061cb019c5c4a33f621
Jose Vives <jota_sk@hotmail.com>
2016-03-12 14:19:26 +0000

    added documentation

--- World.cpp
+++ World.cpp
@@ -1,8 +1,14 @@
 #include "World.h"
 
+/**
+ * Constructor for this class. This initialize all the constants and creates the world
+ *
+ * @arg config: reference to the configuration
+ */
 World::World(const configuration& config) : WORLD_WIDTH(config.worldWidth), WORLD_HEIGHT(config.worldHeight),
 CELL_SIZE(config.cellSize){
 
+    // Create the world on the heap
     world = new util::Cell*[WORLD_WIDTH];
     for(unsigned int i = 0; i < WORLD_WIDTH; i++){
         world[i] = new util::Cell[WORLD_HEIGHT];
@@ -17,15 +23,20 @@ CELL_SIZE(config.cellSize){
 
 }
 
+/**
+ * Destructor for this class. This deallocate the world on the heap
+ */
 World::~World() {
 
-    for(unsigned int i = 0; i < WORLD_WIDTH; i++){
+    // Deallocate the world
+    for(unsigned int i = 0; i < WORLD_WIDTH; i++)
         delete world[i];
-    }
-
     delete [] world;
 }
 
+/**
+ * Print the world on the console
+ */
 void World::printWorld() {
     // Print the world
     for(unsigned int x = 0; x < World::WORLD_WIDTH; x++){
@@ -51,21 +62,49 @@ void World::printWorld() {
     std::cout << std::endl << std::endl;
 }
 
-util::Cell World::getValueAt(int x, int y) {
-    return this -> world[x][y];
+/**
+ * Get the value from one specified cell. This check if the dimension is inside our world dimensions.
+ * If not it return an empty cell. This can be improve by some invalid cell or something similar
+ *
+ * @arg x: the x position
+ * @arg y: the y position
+ *
+ * @return: the value of the selected position
+ */
+util::Cell World::getValueAt(unsigned int x, unsigned int y) {
+    if(x < WORLD_WIDTH && y < WORLD_HEIGHT)
+        return this -> world[x][y];
+    return util::Cell::EMPTY;
 }
 
+/**
+ * Set one cell from our world. This check if the dimension is inside our world dimensions
+ *
+ * @arg x: the x position for the cell we want modified
+ * @arg y: the y position for the cell we want modified
+ * @arg type: the new type for the cell on x and y
+ */
 void World::setValueAt(unsigned int x, unsigned int y, util::Cell type) {
     if(x < WORLD_WIDTH && y < WORLD_HEIGHT)
         this -> world[x][y] = type;
 }
 
+/**
+ * Convert a virtual position to a real coordinate
+ *
+ * @return: the real coordinate
+ */
 double World::virtualToReal(const int &a) {
     double tmp;
     tmp = ((a * ((CELL_SIZE) * (WORLD_WIDTH))) / (WORLD_WIDTH));
     return tmp;
 }
 
+/**
+ * Convert a real coordinate to an virtual coordinate
+ *
+ * @return: the virtual coordinate
+ */
 unsigned int World::realToVirtual(const double &a) {
     unsigned int tmp;
     tmp = (unsigned int) ((WORLD_WIDTH * a) / (CELL_SIZE * (WORLD_WIDTH)));
--- World.h
+++ World.h
@@ -7,24 +7,39 @@
 class World {
 
 public:
+    // Constructor for this class
     World(const configuration&);
+
+    // Destructor for this class
     ~World();
 
+    // The world width
     const unsigned int WORLD_WIDTH;
+
+    // The world height
     const unsigned int WORLD_HEIGHT;
+
+    // The cell size
     const double CELL_SIZE;
 
+    // Print the world
     void printWorld(void);
 
-
+    // Convert virtual coordinates to real coordinates
     double virtualToReal(const int&);
+
+    // Convert real coordinates to virtual coordinates
     unsigned int realToVirtual(const double&);
 
-    util::Cell getValueAt(int, int);
+    // Get the value from a specified cell
+    util::Cell getValueAt(unsigned int, unsigned int);
+
+    // Set the value from a specified cell
     void setValueAt(unsigned int, unsigned int, util::Cell);
 
 private:
 
+    // The world as an 2D array of Cells
     util::Cell** world;
 
 };
