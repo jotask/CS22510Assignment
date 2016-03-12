9c1694cbf36831c1a4507879a05de25dfc4868d2
Jose Vives <jota_sk@hotmail.com>
2016-03-12 14:08:18 +0000

    added documentation

--- Vector2.cpp
+++ Vector2.cpp
@@ -1,37 +1,70 @@
 #include "Vector2.h"
 
+/**
+ * Initialize this class with custom value for the position
+ *
+ * @arg x: The x position
+ * @arg y: The y position
+ */
 Vector2::Vector2(unsigned int x, unsigned int y) {
-    this -> init(x, y);
+    this -> set(x, y);
 }
 
+/**
+ * Default constructor for this class. This initialize this class on a position 0, 0. The defautl position.
+ */
 Vector2::Vector2() {
-    this -> init(0, 0);
+    this -> set(0, 0);
 }
 
+/**
+ * Default destructor
+ */
 Vector2::~Vector2() {
 }
 
-void Vector2::init(unsigned int x, unsigned int y) {
-    this->setX(x);
-    this->setY(y);
-}
-
+/**
+ * Get a copy of the x position
+ *
+ * @return: the x position
+ */
 unsigned int Vector2::getX() {
     return this -> x;
 }
 
+/**
+ * Get a copy of the Y position
+ *
+ * @return: the y position
+ */
 unsigned int Vector2::getY() {
     return this -> y;
 }
 
+/**
+ * Set the x position
+ *
+ * @param x: the new x position
+ */
 void Vector2::setX(unsigned int x) {
     this -> x = x;
 }
 
+/**
+ * Set the y position
+ *
+ * @param y: the new position
+ */
 void Vector2::setY(unsigned int y) {
     this -> y = y;
 }
 
+/**
+ * Set both values at the same time
+ *
+ * @arg x: the new x position
+ * @arg y: the new y position
+ */
 void Vector2::set(unsigned int x, unsigned int y){
     this -> setX(x);
     this -> setY(y);
--- Vector2.h
+++ Vector2.h
@@ -1,23 +1,38 @@
 #ifndef CS22510_VECTOR2_H
 #define CS22510_VECTOR2_H
 
-
+/**
+ * This class holds the coordinates for a 2D world
+ */
 class Vector2 {
 public:
+    // Constructor for this class
     Vector2();
     Vector2(unsigned int, unsigned int);
+
+    // Destructor for this class
     ~Vector2();
 
+    // Get the value on X
     unsigned int getX(void);
+
+    // Get the value on Y
     unsigned int getY(void);
+
+    // Set the value on X
     void setX(unsigned int);
+
+    // Set the value on Y
     void setY(unsigned int);
+
+    // Set X and Y at the same time
     void set(unsigned int, unsigned int);
 
 private:
+
+    // X and Y. Those are unsigned because they can't have negative values
     unsigned int x;
     unsigned int y;
-    void init(unsigned int, unsigned int);
 
 };
 
